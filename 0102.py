a8e60fd1fd4a48b9fb43b5c93c1a20f9deb1107d
explodeTheComputer <dnfjddk2@gmail.com>
2025-01-02 16:36:12 +0900

    sdfsdf

---
+++ 0102.py
@@ -0,0 +1,9 @@
+#사용자 정의 모듈
+def hap(a,b,c=0):
+    result = a+b+c
+    return result
+
+def mul(a,b,c=1):
+    result = a*b*c
+    return result
+
---
+++ test.py
@@ -0,0 +1,3 @@
+numList = [4,7,11,77]
+numList.extend([1,3])
+print(len(numList))
